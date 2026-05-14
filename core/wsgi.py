7db25eb4d75f5329334194ed10627f7453f005a5
Kablai <regexble@gmail.com>
2026-05-14 21:33:27 +0000

    Toujours dans la correction de l'erreur de connection

--- core/wsgi.py
+++ core/wsgi.py
@@ -13,4 +13,15 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-application = get_wsgi_application()
+_application = get_wsgi_application()
+
+def application(environ, start_response):
+    # Apache/mod_wsgi supprime l'en-tête Authorization par défaut.
+    # On le récupère depuis HTTP_X_AUTHORIZATION (header personnalisé)
+    # ou depuis REDIRECT_HTTP_AUTHORIZATION (mod_rewrite).
+    if 'HTTP_AUTHORIZATION' not in environ:
+        if 'HTTP_X_AUTHORIZATION' in environ:
+            environ['HTTP_AUTHORIZATION'] = environ['HTTP_X_AUTHORIZATION']
+        elif 'REDIRECT_HTTP_AUTHORIZATION' in environ:
+            environ['HTTP_AUTHORIZATION'] = environ['REDIRECT_HTTP_AUTHORIZATION']
+    return _application(environ, start_response)
